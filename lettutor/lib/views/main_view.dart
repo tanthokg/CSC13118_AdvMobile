e2580923a97ad507d205f8ad5259c9143142e509
tanthokg <htho379@gmail.com>
2022-10-16 19:08:43 +0700

    Update main_view.dart

--- lettutor/lib/views/main_view.dart
+++ lettutor/lib/views/main_view.dart
@@ -28,119 +28,121 @@ class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: Image.asset('assets/logo.png'),
-          title: Text(
-            'LetTutor',
-            style: Theme.of(context).textTheme.headline1,
-          ),
-          backgroundColor: Colors.white,
-          actions: [
-            PopupMenuButton(
-                icon: const Icon(Icons.translate, color: Colors.blue),
-                itemBuilder: (context) => const [
-                      PopupMenuItem<Language>(
-                        value: Language.english,
-                        child: Text('English'),
-                      ),
-                      PopupMenuItem<Language>(
-                        value: Language.vietnamese,
-                        child: Text('Vietnamese'),
-                      ),
-                    ])
-          ],
+      appBar: AppBar(
+        leading: Image.asset('assets/logo.png'),
+        title: Text(
+          'LetTutor',
+          style: Theme.of(context).textTheme.headline1,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                color: Colors.blue[700],
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 30, horizontal: 12),
-                      child: Text(
-                        'You have no upcoming lesson.',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 26, color: Colors.white),
-                      ),
+        backgroundColor: Colors.white,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.translate, color: Colors.blue),
+            itemBuilder: (context) => const [
+              PopupMenuItem<Language>(
+                value: Language.english,
+                child: Text('English'),
+              ),
+              PopupMenuItem<Language>(
+                value: Language.vietnamese,
+                child: Text('Vietnamese'),
+              ),
+            ],
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.blue[700],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 30, horizontal: 12),
+                    child: Text(
+                      'You have no upcoming lesson.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 24),
-                      child: Text(
-                        'Welcome to LetTutor!',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 24),
+                    child: Text(
+                      'Welcome to LetTutor!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(12, 12, 0, 12),
-                child: Text('Find a tutor', style: Theme.of(context).textTheme.headline3),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 12, 0, 12),
+              child: Text('Find a tutor', style: Theme.of(context).textTheme.headline3),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+              child: Row(
+                children: const [
+                  SizedBox(width: 170, child: TextField()),
+                  SizedBox(width: 20),
+                  SizedBox(width: 170, child: TextField()),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-                child: Row(
-                  children: const [
-                    SizedBox(width: 170, child: TextField()),
-                    SizedBox(width: 20),
-                    SizedBox(width: 170, child: TextField()),
-                  ],
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 12, 0, 12),
+              child: Text('Select available tutoring time:',
+                  style: Theme.of(context).textTheme.headline4),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+              child: Row(
+                children: const [
+                  SizedBox(width: 140, child: TextField()),
+                  SizedBox(width: 20),
+                  SizedBox(width: 200, child: TextField()),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(12, 12, 0, 12),
-                child: Text('Select available tutoring time:',
-                    style: Theme.of(context).textTheme.headline4),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
-                child: Row(
-                  children: const [
-                    SizedBox(width: 140, child: TextField()),
-                    SizedBox(width: 20),
-                    SizedBox(width: 200, child: TextField()),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                child: Wrap(
-                  children: List<Widget>.generate(
-                    filters.length,
-                    (index) => Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 2),
-                      child: ChoiceChip(
-                        label: Text(filters[index]),
-                        selectedColor: Colors.blue,
-                        selected: chosenFilter == index,
-                        onSelected: (bool selected) {
-                          setState(() {
-                            chosenFilter = index;
-                          });
-                        },
-                      ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+              child: Wrap(
+                children: List<Widget>.generate(
+                  filters.length,
+                  (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 2),
+                    child: ChoiceChip(
+                      label: Text(filters[index]),
+                      selectedColor: Colors.blue,
+                      selected: chosenFilter == index,
+                      onSelected: (bool selected) {
+                        setState(() {
+                          chosenFilter = index;
+                        });
+                      },
                     ),
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(12, 0, 0, 12),
-                child: OutlinedButton(
-                  onPressed: () {
-                    setState(() {
-                      chosenFilter = 0;
-                    });
-                  },
-                  child: const Text('Reset Filters'),
-                )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12, 0, 0, 12),
+              child: OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    chosenFilter = 0;
+                  });
+                },
+                child: const Text('Reset Filters'),
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
