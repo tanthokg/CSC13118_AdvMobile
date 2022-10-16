8c4e496714d03e622855032010dad21f49e4d666
tanthokg <htho379@gmail.com>
2022-10-16 19:05:14 +0700

    MainView search bar and filter bar

--- lettutor/lib/main.dart
+++ lettutor/lib/main.dart
@@ -20,14 +20,30 @@ class LetTutor extends StatelessWidget {
         theme: ThemeData(
           primaryColor: Colors.white,
           textTheme: const TextTheme(
-            headline1:
-                TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.blue),
-            headline2:
-                TextStyle(fontSize: 28, fontWeight: FontWeight.w400, color: Colors.blue),
+            headline1: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+            headline2: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w400,
+              color: Colors.blue,
+            ),
+            headline3: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black
+            ),
+            headline4: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black
+            ),
             bodyText1: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
           ),
         ),
-        home: const LoginView(),
+        home: const MainView(),
         routes: {
           Routes.login: (context) => const LoginView(),
           Routes.register: (context) => const RegisterView(),
--- lettutor/lib/views/main_view.dart
+++ lettutor/lib/views/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lettutor/constants/datatype.dart';
 
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
@@ -8,12 +9,138 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  final filters = [
+    'All',
+    'English for kids',
+    'English for Business',
+    'Conversational',
+    'STARTERS',
+    'MOVERS',
+    'FLYERS',
+    'KET',
+    'PET',
+    'IELTS',
+    'TOEFL',
+    'TOEIC'
+  ];
+  int chosenFilter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('This is the main view after logging in successfully'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          leading: Image.asset('assets/logo.png'),
+          title: Text(
+            'LetTutor',
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          backgroundColor: Colors.white,
+          actions: [
+            PopupMenuButton(
+                icon: const Icon(Icons.translate, color: Colors.blue),
+                itemBuilder: (context) => const [
+                      PopupMenuItem<Language>(
+                        value: Language.english,
+                        child: Text('English'),
+                      ),
+                      PopupMenuItem<Language>(
+                        value: Language.vietnamese,
+                        child: Text('Vietnamese'),
+                      ),
+                    ])
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.blue[700],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 30, horizontal: 12),
+                      child: Text(
+                        'You have no upcoming lesson.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 26, color: Colors.white),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 24),
+                      child: Text(
+                        'Welcome to LetTutor!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 12, 0, 12),
+                child: Text('Find a tutor', style: Theme.of(context).textTheme.headline3),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                child: Row(
+                  children: const [
+                    SizedBox(width: 170, child: TextField()),
+                    SizedBox(width: 20),
+                    SizedBox(width: 170, child: TextField()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 12, 0, 12),
+                child: Text('Select available tutoring time:',
+                    style: Theme.of(context).textTheme.headline4),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+                child: Row(
+                  children: const [
+                    SizedBox(width: 140, child: TextField()),
+                    SizedBox(width: 20),
+                    SizedBox(width: 200, child: TextField()),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                child: Wrap(
+                  children: List<Widget>.generate(
+                    filters.length,
+                    (index) => Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2),
+                      child: ChoiceChip(
+                        label: Text(filters[index]),
+                        selectedColor: Colors.blue,
+                        selected: chosenFilter == index,
+                        onSelected: (bool selected) {
+                          setState(() {
+                            chosenFilter = index;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 0, 0, 12),
+                child: OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      chosenFilter = 0;
+                    });
+                  },
+                  child: const Text('Reset Filters'),
+                )
+              ),
+            ],
+          ),
+        ));
   }
 }
