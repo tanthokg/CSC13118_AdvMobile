90d966a47a2b8c98865a52dccc8f5fb4292e08d2
tanthokg <htho379@gmail.com>
2022-10-18 15:54:18 +0700

    Partially finished UserProfileView

--- lettutor/lib/views/user_profile_view.dart
+++ lettutor/lib/views/user_profile_view.dart
@@ -20,128 +20,192 @@ class _UserProfileViewState extends State<UserProfileView> {
         ),
       ),
       body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Stack(
-                  children: [
-                    const CircleAvatar(
-                      radius: 60,
-                      backgroundImage: AssetImage('assets/user-avatar-01.png'),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: CircleAvatar(
-                          backgroundColor: Colors.grey[300],
-                          radius: 18,
-                          child: const Icon(Icons.camera_alt, color: Colors.grey),
-                        ),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/user-avatar-01.png'),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[300],
+                        radius: 18,
+                        child: const Icon(Icons.camera_alt, color: Colors.grey),
                       ),
-                    )
-                  ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child:
+                  Text('Name', style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 4),
+              child: TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child:
-                    Text('Name', style: TextStyle(fontSize: 16, color: Colors.grey[900])),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 4),
-                child: TextField(
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Email Address',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 4),
+              child: TextField(
+                enabled: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[300],
+                  contentPadding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  disabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.5),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Email Address',
-                    style: TextStyle(fontSize: 16, color: Colors.grey[900])),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 4),
-                child: TextField(
-                  enabled: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    contentPadding:
-                        const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                    disabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Country',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 4),
+              child: TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Country',
-                    style: TextStyle(fontSize: 16, color: Colors.grey[900])),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 4),
-                child: TextField(
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Phone Number',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 4),
+              child: TextField(
+                enabled: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[300],
+                  contentPadding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  disabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.5),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Phone Number',
-                    style: TextStyle(fontSize: 16, color: Colors.grey[900])),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 4),
-                child: TextField(
-                  enabled: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    contentPadding:
-                        const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                    disabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 1.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Birthday',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 4),
+              child: TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Birthday',
-                    style: TextStyle(fontSize: 16, color: Colors.grey[900])),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 4),
-                child: TextField(
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                  ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child:
+                  Text('Level', style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 4),
+              child: TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                 ),
               ),
-            ],
-          )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Subjects',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 4),
+              child: TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Test Preparation',
+                  style: TextStyle(fontSize: 16, color: Colors.grey[900])),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 4),
+              child: TextField(
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 2),
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+              ),
+            ),
+            const SizedBox(height: 24),
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
+              ),
+              child: const Text(
+                'SAVE',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
