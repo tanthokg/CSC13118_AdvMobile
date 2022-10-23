b57b508f907a953b4d0eee2cdd26b53194dbc2d8
tanthokg <htho379@gmail.com>
2022-10-23 12:38:28 +0700

    Very basic UI for BecomeTutorView

--- lettutor/lib/views/become_tutor_view.dart
+++ lettutor/lib/views/become_tutor_view.dart
@@ -12,7 +12,72 @@ class _BecomeTutorViewState extends State<BecomeTutorView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Become A Tutor'),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: BackButton(
+          color: Colors.blue[600],
+        ),
+        title: Text(
+          'Become Tutor',
+          style: Theme.of(context).textTheme.headline2,
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Basic Info'),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Image.asset('assets/user-avatar-01.png', width: 100, height: 100,),
+                    TextButton(onPressed: () {}, child: const Text('Upload')),
+                  ],
+                ),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Tutoring Name'),
+                      TextField(),
+                      Text('I am from'),
+                      TextField(),
+                      Text('Date of Birth'),
+                      TextField(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Text('CV'),
+            Text('Interests'),
+            TextField(),
+            Text('Education'),
+            TextField(),
+            Text('Experience'),
+            TextField(),
+            Text('Current or Previous Profession'),
+            TextField(),
+            Text('Certificate'),
+            TextButton(onPressed: () {}, child: Text('Add New Certificate')),
+            Text('Languages I speak'),
+            Text('Languages'),
+            TextField(),
+            Text('Who I teach'),
+            Text('Introduction'),
+            TextField(),
+            Text('I am best at teaching students who are'),
+            TextField(),
+            Text('My specialties are'),
+            TextField(),
+            Text('Introduction Video'),
+            TextButton(onPressed: () {}, child: Text('Choose Video')),
+            TextButton(onPressed: () {}, child: Text('Done')),
+          ],
+      ),
       ),
     );
   }
