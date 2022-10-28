a19c296ee952d258d334c870ee8f9b9e400bae3a
tanthokg <htho379@gmail.com>
2022-10-28 15:51:21 +0700

    Basic UI for TaskCard and complete task action

--- todo_app/lib/pages/all_tasks_page.dart
+++ todo_app/lib/pages/all_tasks_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/constants/routes.dart';
+import 'package:todo_app/constants/dummy_data.dart';
 import 'package:todo_app/entities/task.dart';
+import 'package:todo_app/widgets/task_card.dart';
 
 class AllTasksPage extends StatefulWidget {
   const AllTasksPage({Key? key}) : super(key: key);
@@ -11,14 +12,25 @@ class AllTasksPage extends StatefulWidget {
 
 class _AllTasksPageState extends State<AllTasksPage> {
   List<Task> _fetchAllTasks() {
-    return
+    return allTasks;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('All Tasks'),
+    var tasks = _fetchAllTasks();
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: tasks
+            .map((task) => TaskCard(
+                  task: task,
+                  onCompleteChecked: (value) {
+                    setState(() {
+                      tasks.remove(task);
+                    });
+                  },
+                ))
+            .toList(),
       ),
     );
   }
---
+++ todo_app/lib/widgets/task_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/entities/task.dart';
+
+class TaskCard extends StatelessWidget {
+  const TaskCard({
+    Key? key,
+    required this.task,
+    required this.onCompleteChecked,
+  }) : super(key: key);
+
+  final Task task;
+  final Function(bool?) onCompleteChecked;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0.5,
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          Checkbox(
+            value: false,
+            onChanged: onCompleteChecked,
+          ),
+          Column(
+            children: [
+              Text(task.name),
+              Text(task.dueTime.toString()),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
