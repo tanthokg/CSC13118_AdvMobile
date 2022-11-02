7847b01fa98631185d1194720d9f515b9abbc83d
tanthokg <htho379@gmail.com>
2022-11-02 18:06:03 +0700

    Decorated Drawer in HomePage

--- todo_app/lib/pages/home_page.dart
+++ todo_app/lib/pages/home_page.dart
@@ -26,40 +26,111 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
         drawer: Drawer(
-          child: ListView(
-            //padding: EdgeInsets.zero,
-            children: [
-              ListTile(
-                title: const Text('All Tasks'),
-                onTap: () {
-                  setState(() {
-                    currentSection = Sections.allTasks;
-                  });
-                  Navigator.pop(context);
-                  // Navigator.pushReplacementNamed(context, Routes.all);
-                },
-              ),
-              ListTile(
-                title: const Text('Today'),
-                onTap: () {
-                  setState(() {
-                    currentSection = Sections.today;
-                  });
-                  Navigator.pop(context);
-                  // Navigator.pushNamed(context, Routes.today);
-                },
-              ),
-              ListTile(
-                title: const Text('Upcoming'),
-                onTap: () {
-                  setState(() {
-                    currentSection = Sections.upcoming;
-                  });
-                  Navigator.pop(context);
-                  // Navigator.pushNamed(context, Routes.upcoming);
-                },
-              ),
-            ],
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: MediaQuery.of(context).size.height * 0.075,
+              horizontal: 14,
+            ),
+            child: ListView(
+              //padding: EdgeInsets.zero,
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.list,
+                    color: currentSection == Sections.allTasks
+                        ? Colors.deepOrange
+                        : Colors.black54,
+                  ),
+                  title: Text(
+                    'All Tasks',
+                    style: currentSection == Sections.allTasks
+                        ? const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.deepOrange,
+                          )
+                        : Theme.of(context).textTheme.headline2,
+                  ),
+                  tileColor: currentSection == Sections.allTasks
+                      ? Colors.deepOrange[50]
+                      : Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      currentSection = Sections.allTasks;
+                    });
+                    Navigator.pop(context);
+                    // Navigator.pushReplacementNamed(context, Routes.all);
+                  },
+                ),
+                const SizedBox(height: 8),
+                ListTile(
+                  leading: Icon(
+                    Icons.today,
+                    color: currentSection == Sections.today
+                        ? Colors.deepOrange
+                        : Colors.black54,
+                  ),
+                  title: Text(
+                    'Today',
+                    style: currentSection == Sections.today
+                        ? const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.deepOrange,
+                          )
+                        : Theme.of(context).textTheme.headline2,
+                  ),
+                  tileColor: currentSection == Sections.today
+                      ? Colors.deepOrange[50]
+                      : Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      currentSection = Sections.today;
+                    });
+                    Navigator.pop(context);
+                    // Navigator.pushNamed(context, Routes.today);
+                  },
+                ),
+                const SizedBox(height: 8),
+                ListTile(
+                  leading: Icon(
+                    Icons.upcoming,
+                    color: currentSection == Sections.upcoming
+                        ? Colors.deepOrange
+                        : Colors.black54,
+                  ),
+                  title: Text(
+                    'Upcoming',
+                    style: currentSection == Sections.upcoming
+                        ? const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.deepOrange,
+                          )
+                        : Theme.of(context).textTheme.headline2,
+                  ),
+                  tileColor: currentSection == Sections.upcoming
+                      ? Colors.deepOrange[50]
+                      : Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      currentSection = Sections.upcoming;
+                    });
+                    Navigator.pop(context);
+                    // Navigator.pushNamed(context, Routes.upcoming);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
         body: pages[currentSection],
